df5ce5adfafc616e70f00a9f0e2c88f6c6a0f3bf
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2017-05-01 17:20:25 +0800

    微調

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
 # create and fit the LSTM network
 model = Sequential()
-model.add(LSTM(4, input_shape=(1, look_back))
+model.add(LSTM(4, input_shape=(1, look_back)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(trainX, trainY, epochs=10000, batch_size=1, verbose=2)
